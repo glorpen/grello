f7d0380263a1e1284bb4c10787c06a7f3554658f
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-01-30 12:30:34 +0100

    added simple test

--- src/grello/tests/ui.py
+++ src/grello/tests/ui.py
@@ -4,7 +4,7 @@ Created on 26.01.2017
 @author: glorpen
 '''
 import unittest
-from grello.ui import ConsoleUi
+from grello.ui import ConsoleUi, BaseUi
 from unittest.mock import patch
 
 class TestUi(unittest.TestCase):
@@ -25,3 +25,15 @@ class TestUi(unittest.TestCase):
                 ret = ui.verify_pin("some_url")
                 self.assertTrue(p.write.called)
                 self.assertEqual(ret, v, "Returned value is same as input")
+    
+    def test_abstract(self):
+        ui = BaseUi()
+        
+        # just for coverage
+        
+        with self.assertRaises(NotImplementedError):
+            ui.load_keys()
+        with self.assertRaises(NotImplementedError):
+            ui.save_keys("test", "test")
+        with self.assertRaises(NotImplementedError):
+            ui.verify_pin("test")
